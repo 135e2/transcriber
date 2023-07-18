ccdf973771b57faad1adadcfafd98c8ab4a312be
Mole Shang <135e2@135e2.dev>
2023-07-19 03:19:07 +0800

    fix(transcriber): format timestamps to comply with SRT standard

--- src/transcriber/utils.py
+++ src/transcriber/utils.py
@@ -1,4 +1,5 @@
 import re
+from whisperx.utils import format_timestamp
 
 
 def edit_file_extension(filename, new_ext):
@@ -12,8 +13,10 @@ def write_result(result, o):
     foutput = open(o, "w", encoding="utf-8")
     ct = 0
     for i in result["segments"]:
+        start = format_timestamp(i["start"], True)
+        end = format_timestamp(i["end"], True)
         print(
-            f"{(ct := ct + 1)}\n{i['start']} --> {i['end']}\n{i['text']}\n",
+            f"{(ct := ct + 1)}\n{start} --> {end}\n{i['text']}\n",
             file=foutput,
             flush=True,
         )
