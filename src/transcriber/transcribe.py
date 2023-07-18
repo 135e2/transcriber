39ebe16a901e238d5f64cf4a5603971c6c063627
Mole Shang <135e2@135e2.dev>
2023-07-19 01:50:48 +0800

    fix(transcribe): fallback to int8 if float16 compute_type fails
    Normally this would happen on a CPU device run.

--- src/transcriber/transcribe.py
+++ src/transcriber/transcribe.py
@@ -8,9 +8,12 @@ class WhisperxModel:
     def __init__(self, model_name="large-v2", device="cuda"):
         self.model_name = model_name
         self.device = device
-        self.__model = whisperx.load_model(
-            model_name, device, compute_type=Constants.COMPUTE_TYPE
-        )
+        try:
+            self.__model = whisperx.load_model(
+                model_name, device, compute_type=Constants.COMPUTE_TYPE
+            )
+        except ValueError:
+            self.__model = whisperx.load_model(model_name, device, compute_type="int8")
 
     def load_audio(self, filename):
         self.__audio = whisperx.load_audio(filename)
